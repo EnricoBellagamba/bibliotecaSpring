1783295e44e5c9a76521c20cb8e372092b490442
Margherita Ravanello <margherita.ravanello@gmail.com>
2025-06-14 17:10:41 +0200

    inital commit

---
+++ src/test/java/com/develhope/co/biblioteca_prova/BibliotecaProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develhope.co.biblioteca_prova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaProvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
