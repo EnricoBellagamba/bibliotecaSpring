ab69470bfa7314d3ebed5863f9314a66c58bf789
Margherita Ravanello <margherita.ravanello@gmail.com>
2025-06-15 18:01:22 +0200

    aggiunti metodi findAll e findById in LibriController

--- src/main/java/com/develhope/co/biblioteca_prova/controller/LibriController.java
+++ src/main/java/com/develhope/co/biblioteca_prova/controller/LibriController.java
@@ -1,7 +1,37 @@
 package com.develhope.co.biblioteca_prova.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.develhope.co.biblioteca_prova.models.Libri;
+import com.develhope.co.biblioteca_prova.repository.LibriRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/libri")
 public class LibriController {
+    @Autowired
+    private LibriRepository libriRepo;
+
+    @GetMapping
+    public Page<Libri> findAll(@RequestParam int pageNumber, @RequestParam int pageSize) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        return libriRepo.findAll(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public Libri findById(@PathVariable("id") String isbn) {
+        Optional<Libri> l = libriRepo.findById(isbn);
+        if(l.isPresent()) {
+            return l.get();
+        }
+        throw new ResponseStatusException(HttpStatusCode.valueOf(404));
+    }
+
 }
