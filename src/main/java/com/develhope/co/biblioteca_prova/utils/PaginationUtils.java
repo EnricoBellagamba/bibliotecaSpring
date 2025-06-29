143534be7810369839fd7f5053fa83dfe228b294
Luciano Addeo <lucianoaddeo7@gmail.com>
2025-06-29 17:16:24 +0200

    implemetato ordinamento PaginationDTO

--- src/main/java/com/develhope/co/biblioteca_prova/utils/PaginationUtils.java
+++ src/main/java/com/develhope/co/biblioteca_prova/utils/PaginationUtils.java
@@ -3,6 +3,7 @@ package com.develhope.co.biblioteca_prova.utils;
 import com.develhope.co.biblioteca_prova.dto.PaginationDTO;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 public class PaginationUtils {
 
@@ -17,6 +18,17 @@ public class PaginationUtils {
         if (pg.getPageSize() > 100) {//if separati
             pg.setPageSize(100);
         }
-        return PageRequest.of(pg.getPageNumber(), pg.getPageSize());
+
+        if (pg.getSortDir() == null){
+            pg.setSortDir("ASC");
+        }
+
+        if (pg.getSortField()==null){
+            return PageRequest.of(pg.getPageNumber(), pg.getPageSize());
+
+        }
+        return PageRequest.of(pg.getPageNumber(), pg.getPageSize(),
+                Sort.by(Sort.Direction.fromString(pg.getSortDir()), pg.getSortField())
+        );
     }
 }
