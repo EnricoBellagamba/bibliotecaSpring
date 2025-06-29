94ca43e1ed5845022dfd3a218f53fcb28e151a64
Luciano Addeo <lucianoaddeo7@gmail.com>
2025-06-29 17:38:56 +0200

    Corretto data.sql 'ATTIVO' -> 'IN_ATTESA'. Implementazione getAll in FornitoreController

--- src/main/java/com/develhope/co/biblioteca_prova/controller/FornitoreController.java
+++ src/main/java/com/develhope/co/biblioteca_prova/controller/FornitoreController.java
@@ -1,7 +1,23 @@
 package com.develhope.co.biblioteca_prova.controller;
 
+import com.develhope.co.biblioteca_prova.dto.APIResponse;
+import com.develhope.co.biblioteca_prova.repository.FornitoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/fornitori")
 public class FornitoreController {
+
+    @Autowired
+    FornitoreRepository fornitoreRepo;
+
+    @GetMapping
+    public ResponseEntity<APIResponse> getAll(){
+        APIResponse ar = new APIResponse(fornitoreRepo.findAll());
+        return ResponseEntity.ok(ar);
+    }
 }
