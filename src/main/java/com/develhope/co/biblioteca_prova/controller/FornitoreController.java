eb0213cdc91a7ac946ea092fd790c191fa751bca
Luciano Addeo <lucianoaddeo7@gmail.com>
2025-06-29 18:03:16 +0200

    Implementazione metodi getByName, savein FornitoreController

--- src/main/java/com/develhope/co/biblioteca_prova/controller/FornitoreController.java
+++ src/main/java/com/develhope/co/biblioteca_prova/controller/FornitoreController.java
@@ -1,12 +1,16 @@
 package com.develhope.co.biblioteca_prova.controller;
 
 import com.develhope.co.biblioteca_prova.dto.APIResponse;
+import com.develhope.co.biblioteca_prova.dto.PaginationDTO;
+import com.develhope.co.biblioteca_prova.models.Fornitore;
 import com.develhope.co.biblioteca_prova.repository.FornitoreRepository;
+import com.develhope.co.biblioteca_prova.utils.PaginationUtils;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/fornitori")
@@ -15,9 +19,33 @@ public class FornitoreController {
     @Autowired
     FornitoreRepository fornitoreRepo;
 
+
     @GetMapping
-    public ResponseEntity<APIResponse> getAll(){
-        APIResponse ar = new APIResponse(fornitoreRepo.findAll());
+    public ResponseEntity<APIResponse> getAll(PaginationDTO paginationDTO){
+
+        Pageable pageable = PaginationUtils.createPage(paginationDTO);
+        APIResponse ar = new APIResponse(fornitoreRepo.findAll(pageable));
         return ResponseEntity.ok(ar);
     }
+
+    @GetMapping("/search")
+    public ResponseEntity<APIResponse> getByName(PaginationDTO paginationDTO, @RequestParam String nome){
+
+        Pageable pageable = PaginationUtils.createPage(paginationDTO);
+        APIResponse ar = new APIResponse(fornitoreRepo.findByNomeContains(nome, pageable));
+        return ResponseEntity.ok(ar);
+    }
+
+    @PostMapping
+    public ResponseEntity<APIResponse> save(@Valid @RequestBody Fornitore fornitore, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return ResponseEntity.badRequest().body(new APIResponse("Name can not be null"));
+        }
+
+        Fornitore saved = fornitoreRepo.save(fornitore);
+        return ResponseEntity.ok(new APIResponse(saved));
+    }
+
+
 }
--- src/main/java/com/develhope/co/biblioteca_prova/repository/FornitoreRepository.java
+++ src/main/java/com/develhope/co/biblioteca_prova/repository/FornitoreRepository.java
@@ -1,7 +1,11 @@
 package com.develhope.co.biblioteca_prova.repository;
 
 import com.develhope.co.biblioteca_prova.models.Fornitore;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FornitoreRepository extends JpaRepository<Fornitore, Integer> {
+
+    public Page<Fornitore> findByNomeContains(String nome, Pageable pageable);
 }
