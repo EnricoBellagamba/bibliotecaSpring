16b4216e40616c01a982aef8adff6d01cf81bd67
desan <desanctisisabella95@gmail.com>
2025-07-01 22:41:40 +0200

    created findById method in PrestitoContr

--- src/main/java/com/develhope/co/biblioteca_prova/controller/PrestitoController.java
+++ src/main/java/com/develhope/co/biblioteca_prova/controller/PrestitoController.java
@@ -2,13 +2,18 @@ package com.develhope.co.biblioteca_prova.controller;
 
 import com.develhope.co.biblioteca_prova.dto.APIResponse;
 import com.develhope.co.biblioteca_prova.dto.PaginationDTO;
+import com.develhope.co.biblioteca_prova.models.Prestito;
 import com.develhope.co.biblioteca_prova.repository.PrestitoRepository;
 import com.develhope.co.biblioteca_prova.service.PrestitoService;
 import com.develhope.co.biblioteca_prova.utils.PaginationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class PrestitoController {
@@ -21,5 +26,13 @@ public class PrestitoController {
 
         Pageable pageable = PaginationUtils.createPage(pagination);
         return ResponseEntity.ok().body(new APIResponse(prestitiRepo.findAll(pageable)));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<APIResponse> findById(@PathVariable("id") Integer id) {
+        Optional<Prestito> p = prestitiRepo.findById(id);
+        if (p.isPresent()) {
+            return ResponseEntity.ok().body(new APIResponse(p.get()));
+        }
+        return ResponseEntity.status(404).body(new APIResponse("Prestito non trovato"));
     }
 }
