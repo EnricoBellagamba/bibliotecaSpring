127bb8704ef4debcdea709e5e8822e282d637b21
Luciano Addeo <lucianoaddeo7@gmail.com>
2025-07-02 17:37:13 +0200

    implementazione find all in UtentweController

--- src/main/java/com/develhope/co/biblioteca_prova/controller/UtenteController.java
+++ src/main/java/com/develhope/co/biblioteca_prova/controller/UtenteController.java
@@ -1,7 +1,27 @@
 package com.develhope.co.biblioteca_prova.controller;
 
+import com.develhope.co.biblioteca_prova.dto.APIResponse;
+import com.develhope.co.biblioteca_prova.dto.PaginationDTO;
+import com.develhope.co.biblioteca_prova.repository.UtenteRepository;
+import com.develhope.co.biblioteca_prova.utils.PaginationUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("utenti")
 public class UtenteController {
+
+    @Autowired
+    private UtenteRepository utenteRepo;
+
+    @GetMapping
+    public ResponseEntity<APIResponse> findAll(PaginationDTO paginationDTO){
+
+        Pageable pageable = PaginationUtils.createPage(paginationDTO);
+        return ResponseEntity.ok(new APIResponse(utenteRepo.findAll(pageable)));
+    }
 }
