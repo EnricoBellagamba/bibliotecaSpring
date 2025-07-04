251b5a20b1a11c6e9cf2c41b677611bf9112e492
Luciano Addeo <lucianoaddeo7@gmail.com>
2025-07-04 14:00:53 +0200

    Implementazione metodo save() in PrestitoService, controlli sulla validità delle chiavi esterne

--- src/main/java/com/develhope/co/biblioteca_prova/service/PrestitoService.java
+++ src/main/java/com/develhope/co/biblioteca_prova/service/PrestitoService.java
@@ -1,11 +1,37 @@
 package com.develhope.co.biblioteca_prova.service;
 
+import com.develhope.co.biblioteca_prova.models.Libro;
+import com.develhope.co.biblioteca_prova.models.Prestito;
+import com.develhope.co.biblioteca_prova.models.Utente;
+import com.develhope.co.biblioteca_prova.repository.LibroRepository;
 import com.develhope.co.biblioteca_prova.repository.PrestitoRepository;
+import com.develhope.co.biblioteca_prova.repository.UtenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PrestitoService {
     @Autowired
     private PrestitoRepository prestitoRepo;
+
+    @Autowired
+    private UtenteRepository utenteRepo;
+
+    @Autowired
+    private LibroRepository libroRepo;
+
+    public Prestito save(Prestito prestito){
+        Optional<Utente> utente = utenteRepo.findById(prestito.getUtente().getId());
+        if(utente.isEmpty())
+            throw new DataIntegrityViolationException("Errore di integrità, l'utente non esiste ");
+
+        Optional<Libro> libro = libroRepo.findById(prestito.getLibro().getIsbn());
+        if(libro.isEmpty())
+            throw new DataIntegrityViolationException("Errore di integrità, il libro non esiste ");
+
+        return prestitoRepo.save(prestito);
+    }
 }
