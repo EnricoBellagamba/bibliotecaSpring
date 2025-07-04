c7849ce6026f0982d603b9d1f18bfb69f3bd749b
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2025-07-04 15:39:50 +0200

    aggiunta validazione a numCopie e prezzoPerCopie e fix del metodo save

--- src/main/java/com/develhope/co/biblioteca_prova/controller/AcquistoController.java
+++ src/main/java/com/develhope/co/biblioteca_prova/controller/AcquistoController.java
@@ -2,16 +2,21 @@ package com.develhope.co.biblioteca_prova.controller;
 
 import com.develhope.co.biblioteca_prova.dto.APIResponse;
 import com.develhope.co.biblioteca_prova.dto.PaginationDTO;
+import com.develhope.co.biblioteca_prova.exceptions.DataValidationException;
 import com.develhope.co.biblioteca_prova.models.Acquisto;
 import com.develhope.co.biblioteca_prova.repository.AcquistoRepository;
 import com.develhope.co.biblioteca_prova.repository.LibroRepository;
 import com.develhope.co.biblioteca_prova.service.AcquistoService;
 import com.develhope.co.biblioteca_prova.utils.PaginationUtils;
+import jakarta.validation.Valid;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -44,9 +49,17 @@ public class AcquistoController {
     }
     
     @PostMapping
-    public ResponseEntity<APIResponse> save(@RequestBody Acquisto acquisto){
-        APIResponse response = acquistoService.salvaAcquisto(acquisto);
-        return ResponseEntity.ok(new APIResponse(response.getStatus(),response.getContent(),true));
+    public ResponseEntity<APIResponse> save(@Valid @RequestBody Acquisto acquisto, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            APIResponse apiResponse = new APIResponse(bindingResult.getAllErrors());
+            return ResponseEntity.badRequest().body(apiResponse);
+        }
+        try {
+            return ResponseEntity.ok(new APIResponse(acquistoRepo.save(acquisto)));
+        }catch (DataValidationException | DataIntegrityViolationException e){
+            return ResponseEntity.badRequest().body(new APIResponse(e.getMessage()));
+        }
+
 
     }
 
