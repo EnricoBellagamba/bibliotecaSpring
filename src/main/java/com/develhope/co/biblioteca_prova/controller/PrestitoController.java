0f64f1ad49c1a8c98d523afd3de5217616aa3b5f
Luciano Addeo <lucianoaddeo7@gmail.com>
2025-07-04 13:59:29 +0200

    Implementazione metodo save() in PrestitoController, correzione nome variabile prestitiRepo

--- src/main/java/com/develhope/co/biblioteca_prova/controller/PrestitoController.java
+++ src/main/java/com/develhope/co/biblioteca_prova/controller/PrestitoController.java
@@ -6,33 +6,52 @@ import com.develhope.co.biblioteca_prova.models.Prestito;
 import com.develhope.co.biblioteca_prova.repository.PrestitoRepository;
 import com.develhope.co.biblioteca_prova.service.PrestitoService;
 import com.develhope.co.biblioteca_prova.utils.PaginationUtils;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("prestiti")
 public class PrestitoController {
     @Autowired
-    private PrestitoRepository prestitiRepo;
+    private PrestitoRepository prestitoRepo;
     @Autowired
     private PrestitoService prestitoService;
 
+    @GetMapping
     public ResponseEntity<APIResponse> findAll(PaginationDTO pagination) {
 
         Pageable pageable = PaginationUtils.createPage(pagination);
-        return ResponseEntity.ok().body(new APIResponse(prestitiRepo.findAll(pageable)));
+        return ResponseEntity.ok().body(new APIResponse(prestitoRepo.findAll(pageable)));
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<APIResponse> findById(@PathVariable("id") Integer id) {
-        Optional<Prestito> p = prestitiRepo.findById(id);
+        Optional<Prestito> p = prestitoRepo.findById(id);
         if (p.isPresent()) {
             return ResponseEntity.ok().body(new APIResponse(p.get()));
         }
         return ResponseEntity.status(404).body(new APIResponse("Prestito non trovato"));
+    }
+
+    @PostMapping
+    public ResponseEntity<APIResponse> save(@Valid @RequestBody Prestito prestito, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return ResponseEntity.badRequest().body(new APIResponse(bindingResult.getAllErrors()));
+        }
+        try {
+            return ResponseEntity.ok(new APIResponse(prestitoRepo.save(prestito)));
+        } catch (DataIntegrityViolationException | InvalidDataAccessApiUsageException e) {
+            return ResponseEntity.badRequest().body(new APIResponse(e.getMessage()+ " " + e.getRootCause()));
+        }
     }
 }
