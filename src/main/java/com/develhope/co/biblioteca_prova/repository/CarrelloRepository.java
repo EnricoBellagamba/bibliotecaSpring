e03d265355e3361605256797b409c82e7370c123
Luciano Addeo <lucianoaddeo7@gmail.com>
2025-07-05 19:02:08 +0200

    correzione query jpql per prezzoVendita

--- src/main/java/com/develhope/co/biblioteca_prova/repository/CarrelloRepository.java
+++ src/main/java/com/develhope/co/biblioteca_prova/repository/CarrelloRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CarrelloRepository extends JpaRepository<Carrello, Integer> {
-    @Query("SELECT CAST(SUM(l.prezzo) AS double) FROM Carrello c \n" +
+    @Query("SELECT SUM(l.prezzo)  FROM Carrello c \n" +
             "JOIN c.libro l \n" +
-            "WHERE c.vendita = ?1 \n")
+            "WHERE c.vendita.id = ?1 \n")
     Double prezzoTotale(Integer vendita_id);
 }
