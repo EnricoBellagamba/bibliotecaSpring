34785e7242888ef5b2e931ac96580bf59b7b572f
desan <desanctisisabella95@gmail.com>
2025-07-11 10:05:41 +0200

    deleted prezzoVendita in Fidelitycard, renamed method in Vendita, debug in FidelitycardServ, and updated save method in VenditaController

--- src/main/java/com/develhope/co/biblioteca_prova/service/FidelityCardService.java
+++ src/main/java/com/develhope/co/biblioteca_prova/service/FidelityCardService.java
@@ -3,23 +3,20 @@ package com.develhope.co.biblioteca_prova.service;
 import com.develhope.co.biblioteca_prova.models.FidelityCard;
 import com.develhope.co.biblioteca_prova.models.Libro;
 import com.develhope.co.biblioteca_prova.models.Utente;
+import com.develhope.co.biblioteca_prova.models.Vendita;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FidelityCardService {
 
-    public double calcolaPrezzoFinale(Utente utente, Libro libro) {
-        double prezzoLibro = libro.getPrezzo();
-
-        FidelityCard fidelityCard = utente.getFidelityCard();
-
-        if (fidelityCard != null) {
-            double sconto = fidelityCard.getPrezzo();
-//            double prezzoScontato = prezzoLibro - sconto;
-            return prezzoLibro - sconto;
-        } else {
-            return prezzoLibro;
+    public double calcolaSconto(Vendita vendita) {
+        double sconto = 0;
+        System.out.println("valoreTotale: " + vendita.getValoreTotale());
+        System.out.println("fidelitycard: " + vendita.getUtente().getFidelityCard());
+        if (vendita.getValoreTotale()> 50 && vendita.getUtente().getFidelityCard() != null) {
+            sconto = 0.1;
         }
+        return sconto;
     }
-
+    //scrivere metodo dove si calcola lo sconto di un ordine sopra i 50 euro e un'altro dove l'operatore setta lo sconto
 }
