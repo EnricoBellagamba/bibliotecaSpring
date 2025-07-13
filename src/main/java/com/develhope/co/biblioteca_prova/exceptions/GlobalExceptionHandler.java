fff758f1d42dee7321c3a978396c4b363c76069c
desan <desanctisisabella95@gmail.com>
2025-07-13 19:09:16 +0200

    creazione classe gestione errore parsing LocalDateTime

--- src/main/java/com/develhope/co/biblioteca_prova/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/develhope/co/biblioteca_prova/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,15 @@
 package com.develhope.co.biblioteca_prova.exceptions;
 
+import com.develhope.co.biblioteca_prova.dto.APIResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    public ResponseEntity<APIResponse> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        return ResponseEntity.badRequest()
+                .body(new APIResponse("Formato data non valido. Usa ISO 8601, es. 2025-07-13T10:00:00"));
+    }
 }
