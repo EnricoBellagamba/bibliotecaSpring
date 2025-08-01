f024b839d8ea9017dee4422d23d959e87da0f2b3
Luciano Addeo <lucianoaddeo7@gmail.com>
2025-08-01 20:49:45 +0200

    Metodo per trovare l'ultimo ordine salvato

--- src/main/java/com/develhope/co/biblioteca_prova/repository/AcquistoRepository.java
+++ src/main/java/com/develhope/co/biblioteca_prova/repository/AcquistoRepository.java
@@ -3,7 +3,9 @@ package com.develhope.co.biblioteca_prova.repository;
 import com.develhope.co.biblioteca_prova.models.Acquisto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AcquistoRepository extends JpaRepository<Acquisto, Integer> {
 
-
+    Optional<Acquisto> findTopByOrderByIdDesc();
 }
